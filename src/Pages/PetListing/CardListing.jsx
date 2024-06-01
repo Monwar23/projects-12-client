6becf70c8391def18ea0337bae5879115588ac27
monwar23 <monwar2345@gmail.com>
2024-06-02 01:54:52 +0600

    added petlisting page card

---
+++ src/Pages/PetListing/CardListing.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const CardListing = ({ item }) => {
+  const { pet_image_url, pet_name, pet_age, pet_location, pet_short_description } = item;
+
+  return (
+    <div className=" rounded-md overflow-hidden shadow-lg bg-white mb-4 hover:scale-105 transition">
+      <img className="w-full h-56 object-cover" src={pet_image_url} alt={pet_name} />
+      <div className="px-6 py-4">
+        <div className="font-bold text-center text-pink-500 text-xl mb-2">{pet_name}</div>
+        <p className="text-gray-700 text-center text-base">{pet_short_description}</p>
+      </div>
+      <div className="px-6 py-4 flex justify-center">
+        <span className="inline-block bg-pink-200 rounded-2xl  px-3 py-2 text-sm font-semibold text-gray-700 mr-2">Age : 0{pet_age}</span>
+        <span className="inline-block bg-pink-200 rounded-2xl px-3 py-2 text-sm font-semibold text-gray-700">Location : {pet_location}</span>
+      </div>
+      <div className="flex justify-center mb-4
+      ">
+        <button className="btn btn-outline border-b-4 text-pink-500 hover:bg-pink-500 hover:text-white hover:border-none mr-3 px-6">View Details</button>
+      </div>
+    </div>
+  );
+};
+
+export default CardListing;
--- src/Pages/PetListing/PetListing.jsx
+++ src/Pages/PetListing/PetListing.jsx
@@ -1,6 +1,8 @@
 import { useQuery } from "@tanstack/react-query";
 import useAxiosCommon from "../../hooks/useAxiosCommon";
 import { Helmet } from "react-helmet";
+import CardListing from "./CardListing";
+import LoadingSpinner from "../../shared/LoadingSpinner";
 
 const PetListing = () => {
 
@@ -14,16 +16,21 @@ const PetListing = () => {
         }
     })
 
+    if(isLoading) return <LoadingSpinner></LoadingSpinner>
+
+
     return (
         <div>
             <Helmet>
             <title>LovingPets | PetListing</title>
             </Helmet>
-            <div>
+           <div className="mt-10 mb-5">
+           <div className="grid grid-cols-1 lg:grid-cols-3 gap-6 p-6">
              {
-                category.map(item=><CardCategory key={item._key} item={item}></CardCategory>)
+                pets.map(item=><CardListing key={item._key} item={item}></CardListing>)
             }
         </div>
+           </div>
         </div>
     );
 };
