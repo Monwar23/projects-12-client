21de942d7595cef551db5b8abe09260618bc7db6
monwar23 <monwar2345@gmail.com>
2024-06-06 16:52:36 +0600

    added adoption requested page completely

--- src/Pages/PetListing/DetailsModal.jsx
+++ src/Pages/PetListing/DetailsModal.jsx
@@ -1,49 +1,83 @@
 import { Dialog, DialogPanel, DialogTitle, Transition, TransitionChild } from "@headlessui/react";
 import { Fragment } from "react";
+import useAxiosSecures from "../../hooks/useAxiosSecures";
+import toast from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
 
-const DetailsModal = ({ setIsEditModalOpen ,isOpen,pet,user}) => {
+const DetailsModal = ({ setIsEditModalOpen, isOpen, pet, user }) => {
+  const axiosSecure=useAxiosSecures()
+  const navigate=useNavigate()
+
+  const handleAdopt = async e => {
+    e.preventDefault()
+    const form=e.target
+    const pet_name=pet.pet_name
+    const pet_image_url=pet.pet_image_url
+    const cartIds=pet._id
+    const name=user?.displayName
+    const email=pet.email
+    const requestedEmail=user?.email
+    const phone_number=form.phone_number.value
+    const address=form.address.value
+    const status ='pending'
+
+    const info={pet_name,pet_image_url,cartIds,name,email,requestedEmail,phone_number,address,status}
+    try{
+      const petRes = await axiosSecure.post('/adopt', info)
+        if (petRes.data.insertedId) {
+            // reset()
+            navigate('/petListing')
+           toast.success(`Adopt request sent successfully.`)
+        }
+    }
+    catch(error){
+      toast.error(error.message)
+    }
+  }
+ 
+
   return (
     <Transition appear show={isOpen} as={Fragment}>
-    <Dialog
-      as='div'
-      className='relative z-10'
-      onClose={() => setIsEditModalOpen(false)}
-    >
-      <TransitionChild
-        as={Fragment}
-        enter='ease-out duration-300'
-        enterFrom='opacity-0'
-        enterTo='opacity-100'
-        leave='ease-in duration-200'
-        leaveFrom='opacity-100'
-        leaveTo='opacity-0'
+      <Dialog
+        as='div'
+        className='relative z-10'
+        onClose={() => setIsEditModalOpen(false)}
       >
-        <div className='fixed inset-0 bg-black bg-opacity-25' />
-      </TransitionChild>
+        <TransitionChild
+          as={Fragment}
+          enter='ease-out duration-300'
+          enterFrom='opacity-0'
+          enterTo='opacity-100'
+          leave='ease-in duration-200'
+          leaveFrom='opacity-100'
+          leaveTo='opacity-0'
+        >
+          <div className='fixed inset-0 bg-black bg-opacity-25' />
+        </TransitionChild>
 
-      <div className='fixed inset-0 overflow-y-auto'>
-        <div className='flex min-h-full items-center justify-center p-4 text-center'>
-          <TransitionChild
-            as={Fragment}
-            enter='ease-out duration-300'
-            enterFrom='opacity-0 scale-95'
-            enterTo='opacity-100 scale-100'
-            leave='ease-in duration-200'
-            leaveFrom='opacity-100 scale-100'
-            leaveTo='opacity-0 scale-95'
-          >
-            <DialogPanel className='w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all'>
-              <DialogTitle
-                as='h3'
-                className='text-xl font-medium text-center leading-6 text-pink-500'
-              >
-                {pet.pet_name}
-              </DialogTitle>
-              <div className='mt-2 w-full'>
-              <form className='space-y-4'>
-              <div>
+        <div className='fixed inset-0 overflow-y-auto'>
+          <div className='flex min-h-full items-center justify-center p-4 text-center'>
+            <TransitionChild
+              as={Fragment}
+              enter='ease-out duration-300'
+              enterFrom='opacity-0 scale-95'
+              enterTo='opacity-100 scale-100'
+              leave='ease-in duration-200'
+              leaveFrom='opacity-100 scale-100'
+              leaveTo='opacity-0 scale-95'
+            >
+              <DialogPanel className='w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all'>
+                <DialogTitle
+                  as='h3'
+                  className='text-xl font-medium text-center leading-6 text-pink-500'
+                >
+                  {pet.pet_name}
+                </DialogTitle>
+                <div className='mt-2 w-full'>
+                  <form onSubmit={handleAdopt} className='space-y-4'>
+                    <div>
                       <label className='block text-sm font-medium text-gray-700'></label>
-                      <img  src={pet.pet_image_url} alt={pet.pet_name} className='mt-1 block w-full h-72 rounded-md border-gray-300 shadow-sm' />
+                      <img src={pet.pet_image_url} alt={pet.pet_name} className='mt-1 block w-full h-72 rounded-md border-gray-300 shadow-sm' />
                     </div>
                     <div>
                       <label className='block text-sm font-medium text-gray-700'>Pet Id</label>
@@ -63,7 +97,7 @@ const DetailsModal = ({ setIsEditModalOpen ,isOpen,pet,user}) => {
                         className='mt-1 block p-2 w-full rounded-md border border-pink-500 shadow-sm focus:border-pink-500 focus:ring-pink-500 sm:text-sm'
                       />
                     </div>
-                    
+
                     <div>
                       <label className='block text-sm font-medium text-gray-700'>User Name</label>
                       <input
@@ -85,14 +119,14 @@ const DetailsModal = ({ setIsEditModalOpen ,isOpen,pet,user}) => {
                     <div>
                       <label className='block text-sm font-medium text-gray-700 '>Phone Number</label>
                       <input
-                        type='tel'
+                        type='number' name="phone_number" required
                         className='mt-1 p-2 block w-full rounded-md border border-pink-500 focus:outline-pink-500 shadow-sm focus:border-pink-500 focus:ring-pink-500 sm:text-sm'
                       />
                     </div>
                     <div>
                       <label className='block text-sm font-medium text-gray-700'>Address</label>
                       <input
-                        type='text'
+                        type='text' name="address" required
                         className='mt-1 p-2 block w-full rounded-md border border-pink-500 shadow-sm focus:border-pink-500 focus:ring-pink-500 focus:outline-pink-500 sm:text-sm'
                       />
                     </div>
@@ -106,23 +140,23 @@ const DetailsModal = ({ setIsEditModalOpen ,isOpen,pet,user}) => {
                     </div>
                   </form>
 
-              </div>
-              <hr className='mt-8 ' />
-              <div className='mt-2 flex justify-center'>
-                <button
-                  type='button'
-                  className='inline-flex justify-center rounded-md border border-transparent bg-red-100 px-4 py-2 text-sm font-medium text-pink-500 hover:bg-red-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-red-500 focus-visible:ring-offset-2'
-                  onClick={() => setIsEditModalOpen(false)}
-                >
-                  Cancel
-                </button>
-              </div>
-            </DialogPanel>
-          </TransitionChild>
+                </div>
+                <hr className='mt-8 ' />
+                <div className='mt-2 flex justify-center'>
+                  <button
+                    type='button'
+                    className='inline-flex justify-center rounded-md border border-transparent bg-red-100 px-4 py-2 text-sm font-medium text-pink-500 hover:bg-red-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-red-500 focus-visible:ring-offset-2'
+                    onClick={() => setIsEditModalOpen(false)}
+                  >
+                    Cancel
+                  </button>
+                </div>
+              </DialogPanel>
+            </TransitionChild>
+          </div>
         </div>
-      </div>
-    </Dialog>
-  </Transition>
+      </Dialog>
+    </Transition>
   );
 };
 
--- src/Pages/dashboard/AdoptionRequest.jsx
+++ src/Pages/dashboard/AdoptionRequest.jsx
@@ -1,8 +1,104 @@
+import UseAuth from "../../hooks/UseAuth";
+import useAxiosSecures from "../../hooks/useAxiosSecures";
+import { useQuery } from "@tanstack/react-query";
+import Swal from "sweetalert2";
+import LoadingSpinner from "../../shared/LoadingSpinner";
+import { Helmet } from "react-helmet";
 
 const AdoptionRequest = () => {
+
+    const axiosSecure = useAxiosSecures();
+    const { user } = UseAuth()
+
+    const { data: adopts = [], isLoading, refetch } = useQuery({
+        queryKey: ['adopt', user?.email],
+        queryFn: async () => {
+            const { data } = await axiosSecure(`/adopt/email/${user?.email}`);
+            return data;
+        },
+    });
+    const handleMakeAccepted = adopt => {
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#F472B6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, Accept this Adopt Request!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                axiosSecure.patch(`/adopt/${adopt._id}`)
+                    .then(res => {
+                        // console.log(res.data)
+                        if (res.data.modifiedCount > 0) {
+                            refetch();
+                            Swal.fire({
+                                title: "Updated!",
+                                text: `Request accepted`,
+                                icon: "success"
+                            });
+                        }
+                    })
+            }
+        })
+    }
+
+
+    if (isLoading) return <LoadingSpinner />;
+
+
+
     return (
         <div>
-            <h2>adoption request</h2>
+            <Helmet>
+                <title>LovingPets | Adoption Request</title>
+            </Helmet>
+            <div>
+                <div className="flex justify-evenly my-4">
+                    <h2 className="text-3xl text-center text-pink-500 mb-5">Total Adopt Request : {adopts.length}</h2>
+                </div>
+                <div className="overflow-x-auto">
+                    <table className="table table-zebra w-full">
+                        {/* head */}
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th className="text-center">Pet Name</th>
+                                <th className="text-center">Pet Image</th>
+                                <th className="text-center">Name</th>
+                                <th className="text-center">Email</th>
+                                <th className="text-center">Phone Number</th>
+                                <th className="text-center">Location</th>
+                                <th className="text-center">Status</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                adopts.map((adopt, index) => <tr key={adopt._id}>
+                                    <th className="text-center">{index + 1}</th>
+                                    <td className="text-center">{adopt.pet_name}</td>
+                                    <td className="text-center">
+                                        <img className="rounded-full lg:w-20 w-10 h-10 lg:h-20" src={adopt.pet_image_url} alt="" />
+                                    </td>
+                                    <td className="text-center">{adopt.name}</td>
+                                    <td className="text-center">{adopt.requestedEmail}</td>
+                                    <td className="text-center">{adopt.phone_number}</td>
+                                    <td className="text-center">{adopt.address}</td>
+                                    <td className="text-center">
+                                        {adopt.status === 'accepted' ? 'Accepted' : <button
+                                            onClick={() => handleMakeAccepted(adopt)}
+                                            className="btn border-2 border-pink-500 text-pink-500 hover:text-white hover:bg-pink-500 hover:border-pink-500">
+                                            Pending
+                                        </button>}
+                                    </td>
+                                </tr>)
+                            }
+
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         </div>
     );
 };
