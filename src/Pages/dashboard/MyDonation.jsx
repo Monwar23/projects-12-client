7f1bc2cd94f21f502fc222063a01e0d1b80488ba
monwar23 <monwar2345@gmail.com>
2024-06-08 00:44:37 +0600

    added refund

--- src/Pages/dashboard/MyDonation.jsx
+++ src/Pages/dashboard/MyDonation.jsx
@@ -3,12 +3,11 @@ import useAxiosSecures from "../../hooks/useAxiosSecures";
 import UseAuth from "../../hooks/UseAuth";
 import LoadingSpinner from "../../shared/LoadingSpinner";
 import { Helmet } from "react-helmet";
-import { Link } from "react-router-dom";
+import Swal from "sweetalert2";
 
 const MyDonation = () => {
-
-    const axiosSecure=useAxiosSecures()
-    const {user}=UseAuth()
+    const axiosSecure = useAxiosSecures();
+    const { user } = UseAuth();
 
     const { data: allDonation = [], isLoading, refetch } = useQuery({
         queryKey: ['allDonation', user?.email],
@@ -18,47 +17,71 @@ const MyDonation = () => {
         },
     });
 
-    if(isLoading) return <LoadingSpinner></LoadingSpinner>
+    const handleRefund = payment => {
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#F472B6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, Refund!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                axiosSecure.patch(`/payments/${payment._id}`)
+                    .then(res => {
+                        if (res.data.modifiedCount > 0) {
+                            refetch();
+                            Swal.fire({
+                                title: "Updated!",
+                                text: `Refund Successful`,
+                                icon: "success"
+                            });
+                        }
+                    })
+            }
+        })
+    }
+
+    if (isLoading) return <LoadingSpinner />;
+
+    const filteredDonations = allDonation.filter(donation => donation.donated_amount > 0);
 
     return (
         <div>
-             <Helmet>
+            <Helmet>
                 <title>LovingPets | My Donation</title>
             </Helmet>
             <div className="overflow-x-auto">
-                    <table className="table table-zebra w-full">
-                        <thead>
-                            <tr>
-                                <th>#</th>
-                                <th className="text-center">Pet Name</th>
-                                <th className="text-center">Pet Image</th>
-                                <th className="text-center">Donated Amount</th>
-                                <th className="text-center">Action</th>
+                <table className="table table-zebra w-full">
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th className="text-center">Pet Name</th>
+                            <th className="text-center">Pet Image</th>
+                            <th className="text-center">Donated Amount</th>
+                            <th className="text-center">Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {filteredDonations.map((donation, index) => (
+                            <tr key={donation.id}>
+                                <th className="">{index + 1}</th>
+                                <td className="text-center">{donation.pet_name}</td>
+                                <td className="flex justify-center">
+                                    <img className="rounded-full w-20 h-10 lg:h-20" src={donation.pet_Image_url} alt="" />
+                                </td>
+                                <td className="text-center">${donation.donated_amount}</td>
+                                <td className="text-center">
+                                    <button onClick={() => handleRefund(donation)} className="btn border border-pink-500 text-pink-500 hover:text-white hover:bg-pink-500">
+                                        Refund
+                                    </button>
+                                </td>
                             </tr>
-                        </thead>
-                        <tbody>
-                            {allDonation.map((donation, index) => (
-                                <tr key={donation.id}>
-                                    <th className="">{index + 1}</th>
-                                    <td className="text-center">{donation.pet_name}</td>
-                                    <td className="flex justify-center">
-                                        <img className="rounded-full w-20 h-10 lg:h-20" src={donation.pet_Image_url} alt="" />
-                                    </td>
-                                    <td className="text-center">${donation.donated_amount}</td>
-                                                         
-                                    <td className="text-center">
-                                    <Link >
-                                        <button className="btn border border-pink-500 text-pink-500 hover:text-white hover:bg-pink-500">
-                                            Refund
-                                        </button>
-                                        </Link>
-                                    </td>
-                                   
-                                </tr>
-                            ))}
-                        </tbody>
-                    </table>
-                </div>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
